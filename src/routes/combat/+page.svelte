f0c39fde47c1d88f4dab2a41369f0265207a35c3
Cocostee <clementld76@gmail.com>
2023-12-14 11:49:45 +0100

    Ajout degats

--- src/routes/combat/+page.svelte
+++ src/routes/combat/+page.svelte
@@ -213,7 +213,7 @@
 					class="disabled:bg-red-50 select-none m-2 rounded-lg bg-gradient-to-tr from-gray-900 to-gray-800 py-3 px-6 text-center align-middle font-sans text-xs font-bold uppercase text-white shadow-md shadow-gray-900/10 transition-all hover:shadow-lg hover:shadow-gray-900/20 active:opacity-[0.85]"
 					type="button"
 					on:click={() => effectuerAction(2, index)}
-					disabled={isPlayer1Turn}>{attaque.atkName}</button
+					disabled={isPlayer1Turn}>{attaque.atkName} ({attaque.dmg})</button
 				>
 			{/each}
 		</div>
@@ -229,7 +229,7 @@
 					class="disabled:bg-red-50 select-none m-2 rounded-lg bg-gradient-to-tr from-gray-900 to-gray-800 py-3 px-6 text-center align-middle font-sans text-xs font-bold uppercase text-white shadow-md shadow-gray-900/10 transition-all hover:shadow-lg hover:shadow-gray-900/20 active:opacity-[0.85]"
 					type="button"
 					on:click={() => effectuerAction(1, index)}
-					disabled={!isPlayer1Turn}>{attaque.atkName}</button
+					disabled={!isPlayer1Turn}>{attaque.atkName} ({attaque.dmg})</button
 				>
 			{/each}
 		</div>
